bba7320d98b013418226e373e44b240f97ff0483
Simon Viegas <simon.viegas@outlook.com.br>
2020-10-14 21:13:54 -0300

    Web: Criado páginas Landing e Orphanages

---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+
+import mapMarkerImg from '../images/map-marker.svg';
+
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+  return(
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Salvador</strong>
+          <span>Bahia</span>
+       </footer>
+      </aside>
+
+      <div></div>
+
+      <Map 
+        center={[-13.0104842, -38.5328126]}  
+        zoom={15}
+        style={{ width:'100%', height: '100%' }}        
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF"> </FiPlus>
+      </Link>
+    </div>
+  )
+}
+
+export default OrphanagesMap;
